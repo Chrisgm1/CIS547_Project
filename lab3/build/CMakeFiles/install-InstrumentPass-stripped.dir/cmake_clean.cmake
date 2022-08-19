00d56028400706b893560a567b47ac862bc247c4
Chrisgm1 <97466479+Chrisgm1@users.noreply.github.com>
2022-08-18 23:29:04 -0400

    Delete lab3/build directory

--- lab3/build/CMakeFiles/install-InstrumentPass-stripped.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-InstrumentPass-stripped"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-InstrumentPass-stripped.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
